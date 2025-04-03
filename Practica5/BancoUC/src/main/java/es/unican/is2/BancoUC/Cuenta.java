832ff6dd96aa8cc3e917f0c1231f15ce98a7d214
MonHulk12 <belmontetwo377@gmail.com>
2025-04-03 10:28:08 +0200

    version inicial de aplicación del Banco

---
+++ Practica5/BancoUC/src/main/java/es/unican/is2/BancoUC/Cuenta.java
@@ -0,0 +1,15 @@
+package es.unican.is2.BancoUC;
+
+public class Cuenta {
+	
+	private String numCuenta;
+	
+	public Cuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+	
+	public String getNumCuenta() {
+		return numCuenta;
+	}
+	
+}
---
+++ Practica5/BancoUC/src/main/java/es/unican/is2/BancoUC/saldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package es.unican.is2.BancoUC;
+
+@SuppressWarnings("serial")
+public class saldoInsuficienteException extends RuntimeException {
+
+	public saldoInsuficienteException (String mensaje) {
+		super(mensaje);
+	}
+}
